d6b0d40664c5854611023f1477ebdbe339db19a1
Yevgeny <devlev1980@gmail.com>
2018-11-28 16:13:09 +0200

    change to const let

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require("express");
-var request = require("request");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-var db = mongoose.connect('mongodb://yevgenyl:papa2207@ds119024.mlab.com:19024/movies');
-var Movie = require("./models/movie");
+mLabUrl ='mongodb://yevgenyl:papa2207@ds119024.mlab.com:19024/movies';
+// mongoose.connect("mongodb://localhost:27017/YourDB", { useNewUrlParser: true });
+const db = mongoose.connect(mLabUrl, { useNewUrlParser: true });
+const Movie = require("./models/movie");
 
-var app = express();
+const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
@@ -50,8 +52,8 @@ app.post("/webhook", function (req, res) {
 });
 
 function processPostback(event) {
-    var senderId = event.sender.id;
-    var payload = event.postback.payload;
+    const senderId = event.sender.id;
+    const payload = event.postback.payload;
 
     if (payload === "Greeting") {
         // Get user's first name from the User Profile API
@@ -64,19 +66,19 @@ function processPostback(event) {
             },
             method: "GET"
         }, function(error, response, body) {
-            var greeting = "";
+            let greeting = "";
             if (error) {
                 console.log("Error getting user's name: " +  error);
             } else {
-                var bodyObj = JSON.parse(body);
-                var name = bodyObj.first_name;
-                greeting = "Hi " + name + ". ";
+                const bodyObj = JSON.parse(body);
+                const name = bodyObj.first_name;
+                 greeting = "Hi " + name + ". ";
             }
-            var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+            const message = greeting + "My name is SP Movie Bot. I can tell you constious details regarding movies. What movie would you like to know about?";
             sendMessage(senderId, {text: message});
         });
     } else if (payload === "Correct") {
-        sendMessage(senderId, {text: "Awesome! What would you like to find out? Enter 'plot', 'date', 'runtime', 'director', 'cast' or 'rating' for the various details."});
+        sendMessage(senderId, {text: "Awesome! What would you like to find out? Enter 'plot', 'date', 'runtime', 'director', 'cast' or 'rating' for the constious details."});
     } else if (payload === "Incorrect") {
         sendMessage(senderId, {text: "Oops! Sorry about that. Try using the exact title of the movie"});
     }
@@ -84,15 +86,15 @@ function processPostback(event) {
 
 function processMessage(event) {
     if (!event.message.is_echo) {
-        var message = event.message;
-        var senderId = event.sender.id;
+        let message = event.message;
+        let senderId = event.sender.id;
 
         console.log("Received message from senderId: " + senderId);
         console.log("Message is: " + JSON.stringify(message));
 
         // You may get a text or attachment but not both
         if (message.text) {
-            var formattedMsg = message.text.toLowerCase().trim();
+            let formattedMsg = message.text.toLowerCase().trim();
 
             // If we receive a text message, check to see if it matches any special
             // keywords and send back the corresponding movie detail.
@@ -119,10 +121,10 @@ function processMessage(event) {
 function findMovie(userId, movieTitle) {
     request("http://www.omdbapi.com/?type=movie&t=" + movieTitle, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            var movieObj = JSON.parse(body);
+            const movieObj = JSON.parse(body);
             if (movieObj.Response === "True") {
-                var query = {user_id: userId};
-                var update = {
+                let query = {user_id: userId};
+                let update = {
                     user_id: userId,
                     title: movieObj.Title,
                     plot: movieObj.Plot,
@@ -133,7 +135,7 @@ function findMovie(userId, movieTitle) {
                     rating: movieObj.imdbRating,
                     poster_url:movieObj.Poster
                 };
-                var options = {upsert: true};
+                let options = {upsert: true};
                 Movie.findOneAndUpdate(query, update, options, function(err, mov) {
                     if (err) {
                         console.log("Database error: " + err);
